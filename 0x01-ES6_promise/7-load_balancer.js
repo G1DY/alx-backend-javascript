45fd618b634f4475f1909baccb3f25490ca1f78e
gideon <kipkerring.gedion@gmail.com>
2023-09-30 01:39:54 +0300

    function loadbalancer

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,13 +1,8 @@
-// export default function loadBalancer(chinaDownload, USDownload) {
-//   return Promise
-//     .race([chinaDownload, USDownload])
-//     .then((res) => res);
-// }
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.race([chinaDownload, USDownload])
     .then((res) => res)
     .catch((error) => {
       console.error('Load balancing error:', error);
-      throw error; // Re-throw the error to propagate it
+      throw error;
     });
 }
