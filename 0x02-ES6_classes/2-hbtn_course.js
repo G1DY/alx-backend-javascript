54b6d80e09964439226ba9228b698583548d2527
gideon <kipkerring.gedion@gmail.com>
2023-10-19 12:16:13 +0300

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
+    this.name = 'name';
     this.length = length;
-    this.students = students;
+    this.students = 'students';
   }
   
   get name() {
