61d6f3ce0b3532cdd84074c98f32fe6062f109c1
gideon <kipkerring.gedion@gmail.com>
2023-10-19 11:20:36 +0300

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,34 +10,31 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if ((typeof name !== 'string') && !(name instanceof String)) {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
 
-  }
-  
   get length() {
     return this._length;
   }
 
   set length(length) {
-    if ((typeof length !== 'number') && !(length instanceof Number)) {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
 
-  }
-  
   get students() {
     return this._students;
   }
 
   set students(students) {
-    if (!(students instanceof Array) || !students.every((s) => typeof s == 'string')) {
+    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = students;
   }
+}
