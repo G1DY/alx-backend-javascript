1c5295a82e50f70474e1835a191411b9e61de836
Gideon <kipkerring.gedion@gmail.com>
2023-11-21 15:42:31 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,12 +7,12 @@
  * This important software is now closing
  */
 
-process.stdout._write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  process.stdout._write(`Your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout._write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
