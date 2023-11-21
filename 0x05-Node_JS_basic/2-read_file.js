00c6b991a01470fb2e3a8877233c7c4c92f1f30f
Gideon <kipkerring.gedion@gmail.com>
2023-11-21 17:00:05 +0300

    2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,54 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
+/**
+ * function countStudents that accepts a path in argument
+ * The script should attempt to read the database file synchronously
+ * If the database is not available, it should throw an error
+ * with the text Cannot load the database
+ * If the database is available, it should log the following
+ * message to the console Number of students: NUMBER_OF_STUDENTS
+ * It should log the number of students in each field, and the list
+ * with the following format: Number of students in FIELD: 6.
+ * List: LIST_OF_FIRSTNAMES
+ * CSV file can contain empty lines (at the end) -
+ * and they are not a valid student!
+ */
+
+import { existsSync, statSync, readFileSync } from 'fs';
+
+const countStudents = (dataPath) => {
+  if (!existsSync(dataPath)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!statSync(dataPath).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const fileLines = readFileSync(dataPath, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
+  const studentGroups = {};
+  const dbFieldNames = fileLines[0].split(',');
+  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+  for (const line of fileLines.slice(1)) {
+    const studentRecord = line.split(',');
+    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    const field = studentRecord[studentRecord.length - 1];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
+    }
+    const studentEntries = studentPropNames
+      .map((propName, idx) => [propName, studentPropValues[idx]]);
+    studentGroups[field].push(Object.fromEntries(studentEntries));
+  }
+
+  const totalStudents = Object
+    .values(studentGroups)
+    .reduce((pre, cur) => (pre || []).length + cur.length);
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+  }
+};
