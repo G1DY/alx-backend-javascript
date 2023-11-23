55eebc9cde7fc47774a58d26457c31ead7377a5f
Gideon <kipkerring.gedion@gmail.com>
2023-11-23 14:41:08 +0300

    0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,31 +1,31 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+import { strictEqual, fail } from 'assert';
+import calculateNumber from './0-calcul';
 
 describe('calculateNumber', () => {
   it('should round a and b and return their sum', () => {
-    assert.strictEqual(calculateNumber(0.5, 2.5), 4);
-    assert.strictEqual(calculateNumber(1.4, 2.7), 4);
+    strictEqual(calculateNumber(0.5, 2.5), 4);
+    strictEqual(calculateNumber(1.4, 2.7), 4);
   });
   it('should fail if a or b is not a number', () => {
     try {
       calculateNumber('not a number', 2.5);
-      assert.fail('Expected calculateNumber to throw an error for invalid argument');
+      fail('Expected calculateNumber to throw an error for invalid argument');
     } catch (error) {
-      assert.strictEqual(error.name, 'AssertionError');
+      strictEqual(error.name, 'AssertionError');
     }
 
     try {
       calculateNumber(2.5, 'not a number');
-      assert.fail('Expected calculateNumber to throw an error for invalid argument');
+      fail('Expected calculateNumber to throw an error for invalid argument');
     } catch (error) {
-      assert.strictEqual(error.name, 'AssertionError');
+      strictEqual(error.name, 'AssertionError');
     }
 
     try {
       calculateNumber('not a number', 'not number');
-      assert.fail('Expected calculateNumber to throw an error for invalid arguments');
+      fail('Expected calculateNumber to throw an error for invalid arguments');
     } catch (error) {
-      assert.strictEqual(error.name, 'AssertionError');
+      strictEqual(error.name, 'AssertionError');
     }
   });
 });
