a625e5ba560d0cbf4ab8d40176cd8e2dd6bc1f24
Gideon <kipkerring.gedion@gmail.com>
2023-11-23 12:38:15 +0300

    0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,8 +7,8 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1.4, 2.7), 4);
   });
   it('should fail if a or b is not a number', () => {
-    assert.throws(() => calculateNumber('not a number', 2.5), TypeError);
-    assert.throws(() => calculateNumber(2.5, 'not a number'), TypeError);
-    assert.throws(() => calculateNumber('not a number', 'not number'), TypeError);
+    assert.throws(() => calculateNumber('not a number', 2.5), TypeError, 'a is not a number');
+    assert.throws(() => calculateNumber(2.5, 'not a number'), TypeError, 'b is not a number');
+    assert.throws(() => calculateNumber('not a number', 'not number'), TypeError, 'a and b are not numbers');
   })
 });
