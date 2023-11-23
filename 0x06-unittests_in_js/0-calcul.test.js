acef457f7bf53825efc5d0546e7620f51169393d
Gideon <kipkerring.gedion@gmail.com>
2023-11-23 11:13:19 +0300

    0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should round a and b and return their sum', () => {
-    assert.Equal(calculateNumber(1, 3.5), 4);
-    assert.Equal(calculateNumber(1.4, 2.5), 4);
+    assert.strictEqual(calculateNumber(0.5, 2.5), 4);
+    assert.strictEqual(calculateNumber(1.4, 2.7), 4);
   });
 });
