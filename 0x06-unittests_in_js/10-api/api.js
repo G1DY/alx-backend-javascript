95fc8c0a0650d395760d943bae606fca108f9e5c
Gideon <kipkerring.gedion@gmail.com>
2023-11-24 17:32:02 +0300

    api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -4,6 +4,7 @@ const HOST = 'localhost';
 const PORT = 7865;
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system',);
@@ -20,18 +21,14 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   }
 });
 
-app.get('/available_payments', (req, res) => {
-  res.send({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    },
-  });
-});
-
 app.post('/login', (req, res) => {
   const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+
+  if (userName) {
+    res.send(`Welcome ${userName}`);
+  } else {
+    res.status(400).send('Bad Request: Please provide a valid username');
+  }
 });
 
 app.listen(PORT, HOST, () => {
