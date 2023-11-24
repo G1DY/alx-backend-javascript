97255cb04bf374bfd83a6f4700e35e8568315962
Gideon <kipkerring.gedion@gmail.com>
2023-11-24 14:20:19 +0300

    api.test.js

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -4,35 +4,33 @@ const request = require('request');
 const HOST = 'localhost';
 const PORT = 7865;
 
-describe('API Integration', () => {
-  const API_URL = 'http://localhost:7865';
-
-  it ('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
-      done();
+describe('Express app test suite', function() {
+  describe('/', function() {
+    it('should return home page', function(done) {
+      request.get(`http://${HOST}:${PORT}/`, (error, res, body) => {
+        if (error) expect(res.statusCode).to.not.equal(200);
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
   });
 
-  it ('GET /cart/:id returns correct response for valid :id', (done) => {
-    request.get(`${API_URL}/cart/12`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(404);
-      expect(body).to.be.equal('Payment methods for cart at 12');
-      done();
+  describe('/cart', function() {
+    it('should return cart page with cart id', function(done) {
+      request.get(`http://${HOST}:${PORT}/cart/12`, (error, res, body) => {
+        if (error) expect(res.statusCode).to.not.equal(200);
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
     });
-  });
-  it ('GET /cart/:id returns 404 response for negative numbers valuesin :id', (done) => {
-    request.get(`${API_URL}/cart/-12`, (_err, res, _body) => {
-      expect(res.statusCode).to.be.equal(404);
-      done();
-    });
-  });
-
-  it ('GET /cart/:id returns 404 response for non-numeric values of :id', (done) => {
-    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
-      expect(res.statusCode).to.be.equal(404);
-      done();
+    it('should return an error if :id parameter is not a number', function(done) {
+      request.get(`http://${HOST}:${PORT}/cart/gi`, (error, res, body) => {
+        if (error) expect(res.statusCode).to.not.equal(200);
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
     });
   });
 });
