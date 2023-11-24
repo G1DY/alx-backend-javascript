12527c8ec6b866734280234e3be1299f1a817c43
Gideon <kipkerring.gedion@gmail.com>
2023-11-24 12:18:02 +0300

    api.js

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system',);
 });
 
+app.get('/cart/:id(\\d+)', (res, req) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
 app.listen(PORT, HOST, () => {
   console.log(`API available on ${HOST} port ${PORT}`);
 });
